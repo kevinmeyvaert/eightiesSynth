49111add51bce10348c30b708e47c9610932a849
kevinmeyvaert <kevin.meyvaert@gmail.com>
2016-11-28 18:01:22 +0100

    bind controller + read note

--- src/js/containers/App.jsx
+++ src/js/containers/App.jsx
@@ -4,21 +4,31 @@ import WebMidi from 'webmidi';
 class App extends Component {
 
   componentDidMount() {
-    WebMidi.enable(function (err) {
-
+    WebMidi.enable(err => {
       if (err) {
         console.log(`WebMidi could not be enabled.`, err);
       } else {
         console.log(`WebMidi enabled!`);
+        this.initMidiControls();
       }
-
     });
+  }
+
+  initMidiControls() {
+    const input = WebMidi.getInputByName(`Keystation Mini 32`);
+    input.addListener(`noteon`, `all`, e => {
+      this.handlePlayNote(e);
+    });
+  }
+
+  handlePlayNote(e) {
+    console.log(`Received 'noteon' message (${  e.note.name  }${e.note.octave  }).`);
   }
 
   render() {
     return (
         <main>
-          HALLO KUT
+          Super C00le App van KEVIN en ook een beetje van Anthony.
         </main>
     );
   }
