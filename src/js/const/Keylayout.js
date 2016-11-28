cf5963f19be8a1a84725a9df5d017e6821f2b392
AnthonyMagnus <Anthony@MacBook-Pro-van-Anthony.local>
2016-11-28 21:19:19 +0100

    Keyboard on screen

--- src/js/const/Keylayout.js
+++ src/js/const/Keylayout.js
@@ -4,11 +4,127 @@ const Keylayout = [
     color: `white`
   },
   {
+    note: `C#0`,
+    color: `black`
+  },
+  {
     note: `D0`,
     color: `white`
   },
   {
+    note: `D#0`,
+    color: `black`
+  },
+  {
     note: `E0`,
+    color: `white`
+  },
+  {
+    note: `F0`,
+    color: `white`
+  },
+  {
+    note: `F#0`,
+    color: `black`
+  },
+  {
+    note: `G0`,
+    color: `white`
+  },
+  {
+    note: `G#0`,
+    color: `black`
+  },
+  {
+    note: `A0`,
+    color: `white`
+  },
+  {
+    note: `A#0`,
+    color: `black`
+  },
+  {
+    note: `B0`,
+    color: `white`
+  },
+  {
+    note: `C1`,
+    color: `white`
+  },
+  {
+    note: `C#1`,
+    color: `black`
+  },
+  {
+    note: `D1`,
+    color: `white`
+  },
+  {
+    note: `D#1`,
+    color: `black`
+  },
+  {
+    note: `E1`,
+    color: `white`
+  },
+  {
+    note: `F1`,
+    color: `white`
+  },
+  {
+    note: `F#1`,
+    color: `black`
+  },
+  {
+    note: `G1`,
+    color: `white`
+  },
+  {
+    note: `G#1`,
+    color: `black`
+  },
+  {
+    note: `A1`,
+    color: `white`
+  },
+  {
+    note: `A#1`,
+    color: `black`
+  },
+  {
+    note: `B1`,
+    color: `white`
+  },
+  {
+    note: `C2`,
+    color: `white`
+  },
+  {
+    note: `C#2`,
+    color: `black`
+  },
+  {
+    note: `D2`,
+    color: `white`
+  },
+  {
+    note: `D#2`,
+    color: `black`
+  },
+  {
+    note: `E2`,
+    color: `white`
+  },
+  {
+    note: `F2`,
+    color: `white`
+  },
+  {
+    note: `F#2`,
+    color: `black`
+  },
+  {
+    note: `G2`,
     color: `white`
   }
 ];
